30423f5e4a9a99fa8e99065f241e3b13f3b68f37
Dylski <dylski@dylski>
2018-04-03 17:28:57 +1000

    Add project files.

---
+++ PS2DSP/HexBlock.cpp
@@ -0,0 +1,44 @@
+#include <istream>
+#include <ostream>
+#include "HexBlock.h"
+using namespace std;
+
+HexBlock::HexBlock()
+{
+	for (int i = 0; i < 15; i++)
+	{
+		fBuffer[i] = 0;
+	}
+	fSize = 0;
+}
+
+istream & HexBlock::read(istream & aIStream)
+{
+	// May need this
+	\
+	// for loop may be needed her to read up to 16 characters into the buffer
+	for (int i = 0; i < aIStream.fBuffer; i++)
+	{
+		aIStream >> aIStream.fBuffer[i];
+		// do i need a counter here to record the numbver of characters read in size
+		// maybe aIStream.size++
+	}
+	return aIStream;
+}
+
+unsigned long HexBlock::size() const
+{
+	HexBlock BufferSize;
+	return sizeof(BufferSize.fBuffer); // this may not work
+}
+
+ostream & operator<<(std::ostream & aOStream, const HexBlock & aObject)
+{
+	// make a for loop that goes through the buffer then out put it to aOStream
+	for (int i = 0; i < aObject.fBuffer; i++)
+	{
+		aOStream << aObject.fBuffer[i]; // this is probs wrong
+	
+	}
+	return aOStream;
+}
